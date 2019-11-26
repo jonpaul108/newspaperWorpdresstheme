ff5f48629e1458d715a97df957013a506bef4eb5
Jonathan McNamara <jpmcnmr@gmail.com>
2019-11-26 12:47:07 -0700

    linked banner button to latest post, started building store page

--- page-store.php
+++ page-store.php
@@ -5,4 +5,12 @@
 get_header();
 ?>
 
+<div class='storeContainer'>
+
+    <div class='heroContainer'>
+    </div>
+</div>
+
+<?php echo get_post_field('post_content', 59); ?>
+
 <?php get_footer();?>
