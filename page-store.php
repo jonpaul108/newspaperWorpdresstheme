55c97e284e9913e4d0c9d6d18b519f2654b622bf
Jonathan McNamara <jpmcnmr@gmail.com>
2019-11-28 05:13:20 -0700

    store page text, and repostitioning shirt content

--- page-store.php
+++ page-store.php
@@ -20,26 +20,41 @@ get_header('new');
     <aside class='storeMenu' > 
         <?php wp_nav_menu(array('theme_location' => 'store-nav')) ?>
     </aside>
+    <div class='shirtText'>
+             <p>
+                Bringing you rad and awesome animal themed and world inspired t-shirts.
+            </p>
+            <div></div>
+        <p>
+        Funny and cool animal themed tees.<br>
+        Straight from our content creators, you won't find these designs anywhere else.<br> 
+        And the best news? 20% of profits goes to support the environment.<br>
+        Mens, womens, childrens. Check out our collection today.
+        </p>
+        <p>New Shirts</p>
+        <div></div>
+    </div>
     <div class='shirtContainer'>
         <?php 
         global $post;
-     
         $myposts = get_posts( array(
             'numberposts' => 10,
-                'post_type'   => 'Shirts',
+            'post_type'   => 'Shirts',
         ) );
-        
-
               if ($myposts) :
                   foreach($myposts as $post):
                         setup_postdata($post); ?>
+                    <div class='shirtListing'>
                         <h2>
                             <a href='<?php the_permalink();?>' >
                                 <?php the_title(); ?>
                             </a>
                         </h2>
-                        <?php the_content(); ?>
-                
+                        <?php the_post_thumbnail(); ?>
+                        <div class='shirtPrice'>
+                            <?php echo get_post_meta($post->ID, 'Price', true); ?>
+                        </div>
+                  </div>  
          <?php
               endforeach; 
                 wp_reset_postdata();
@@ -49,7 +64,5 @@ get_header('new');
         ?>
     </div>
 </div>
-
-<?php echo get_post_field('post_content', 59); ?>
 
 <?php get_footer();?>
